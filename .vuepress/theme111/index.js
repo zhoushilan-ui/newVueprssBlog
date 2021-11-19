eec21a9324896761622561caa69728eab844327e
zhoshilan <1846603334@qq.com>
2021-11-19 17:23:38 +0800

    添加文档

---
+++ .vuepress/theme111/index.js
@@ -0,0 +1,23 @@
+// module.exports = {
+//   title: "蓝懒懒的博客",
+//   description: "",
+//   base: "/newVueprssBlog/", // 这是部署到github相关的配置
+//   markdown: {
+//     lineNumbers: true, // 代码块显示行号
+//   },
+//   head: [
+//     // 注入到当前页面的 HTML <head> 中的标签
+//     ["link", { rel: "icon", href: "/dog.jpg" }], // 增加一个自定义的 favicon(网页标签的图标)
+//   ],
+//   pages: [
+//     {
+//       lastUpdated: 1524027677000,
+//       path: "/",
+//       title: "VuePress",
+//       frontmatter: {},
+//     },
+//   ],
+//   extend: "@vuepress/theme-default",
+//   globalLayout: "/path/to/your/global/vue/sfc",
+//   plugins: ["@vuepress/active-header-links"],
+// };
